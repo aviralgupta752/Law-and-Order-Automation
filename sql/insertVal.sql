c8f75c4fd3622437f3a4fdc6aa9df3d18f2a67f0
Bhupesh <66639467+BSardana01@users.noreply.github.com>
2021-04-29 14:52:44 +0530

    Add files via upload

---
+++ sql/insertVal.sql
@@ -0,0 +1,6 @@
+insert into administrator values ('PD001');
+insert into police_dept values ('PD001', 'allahabad', 'iiita','PO001','AP001','CR001','FIR001');
+insert into fir (FIR_NAME, FIR_FNAME, FIR_EMAIL, FIR_CONTACT, FIR_DOI, FIR_POI, FIR_DEP, FIR_DESC) values ('aviral', 'bhupesh', 'aviral@gmail.com', '9999999999', '12022001', 'abcd', 'Women Protection', 'chal raha h ye');
+insert into police_officer values ('PO001', 'nagabhosdika', '12031857', 'naga@gmail.com', '9999999999', 'random address', 'Male', 'Law and Order');
+insert into criminal values ('CR001', 'nagamurder', 'ansh', '20', 'hbnfg','PD001');
+insert into applic values ('AP001', 'edbne', 'mbnfm','hbnv','PO001');
