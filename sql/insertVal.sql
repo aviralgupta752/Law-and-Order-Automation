28b90f63858b677435559620be71c102243f60aa
Bhupesh <66639467+BSardana01@users.noreply.github.com>
2021-05-01 14:45:04 +0530

    Update insertVal.sql

--- sql/insertVal.sql
+++ sql/insertVal.sql
@@ -1,6 +1,7 @@
-insert into administrator values ('PD001');
-insert into police_dept values ('PD001', 'allahabad', 'iiita','PO001','AP001','CR001','FIR001');
-insert into fir (FIR_NAME, FIR_FNAME, FIR_EMAIL, FIR_CONTACT, FIR_DOI, FIR_POI, FIR_DEP, FIR_DESC) values ('aviral', 'bhupesh', 'aviral@gmail.com', '9999999999', '12022001', 'abcd', 'Women Protection', 'chal raha h ye');
-insert into police_officer values ('PO001', 'nagabhosdika', '12031857', 'naga@gmail.com', '9999999999', 'random address', 'Male', 'Law and Order');
+insert into administrator values ('1');
+INSERT INTO police_dept (PD_NAME, PD_LOC, PD_CITY, PD_AREA, PD_PHONE, PD_REMARKS) VALUES ('Sirsa Mandi Police Station', 'Grid-4', 'Sirsa','Anaj Mandi Sirsa','9899898765','Some text about the police station');
+INSERT INTO fir (FIR_NAME, FIR_FNAME, FIR_EMAIL, FIR_CONTACT, FIR_DOI, FIR_PS, FIR_DEP, FIR_DESC, FIR_STAT, FIR_EMAIL_SENT, FIR_PO_ID) VALUES ('Test', 'FatherTest', 'visitormanagementsystem23@gmail.com', '9999999999', '12022001', 'Sirsa Mandi Police Station', 'Cybercrime', 'This complaint is registered with regard to a fraud bank payment on my SBI debit card.', 0, 0, '1');
+INSERT INTO police_officer (PO_NAME, PO_DOB, PO_GENDER, PO_DEP, PO_EMAIL, PO_CONTACT, PO_PS, PO_DRUGS, PO_AID, PO_LENSES) VALUES ('TestPoliceOfficer', '12031987', 'Male', 'Cybercrime', 'test@gmail.com', '9898989897', 'Sirsa Mandi Police Station', 'No', 'No', 'Yes');
 insert into criminal values ('CR001', 'nagamurder', 'ansh', '20', 'hbnfg','PD001');
 insert into applic values ('AP001', 'edbne', 'mbnfm','hbnv','PO001');
+INSERT INTO police_officer_list values ('TestPoliceOfficer', 'test');
