64dd80cc79274fdf6ea0b2f2b285fffa74d6cde1
Bhupesh <66639467+BSardana01@users.noreply.github.com>
2021-05-05 14:29:25 +0530

    Update mainpage.java

--- src/mainpage.java
+++ src/mainpage.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.lang.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.UUID;
 
 import kanoon_ke_haath.admin_login;
 import kanoon_ke_haath.about;
@@ -26,8 +32,10 @@ class mainpage
 	static JButton aboutBtn, quitBtn, admin_loginBtn, police_loginBtn, register_complaintBtn, register_applicationBtn, track_complaintBtn;
         
         static Connection con;
-        static String connectionString = "jdbc:hsqldb:file:db_data/database;hsqldb.lock_file=false";
-	public static void display() throws IOException
+//        static String connectionString = "jdbc:mysql://65.1.1.8:3306/test\",\"police\",\"Policemgmt@7police";
+//        static String connectionString = "jdbc:hsqldb:file:db_data/database;hsqldb.lock_file=false";
+
+	public void display() throws IOException
 	{
 		frame = new JFrame("Main Screen");
 		panel = new JPanel(new BorderLayout(10,10));
@@ -54,7 +62,8 @@ class mainpage
 		quitBtn.addActionListener(new CustomActionListener());
 		register_complaintBtn.addActionListener(new CustomActionListener());
                 register_applicationBtn.addActionListener(new CustomActionListener());
-
+                track_complaintBtn.addActionListener(new CustomActionListener());
+                
 		quitBtn.setBackground(new Color(255,92,96));
 		
 		
@@ -71,7 +80,7 @@ class mainpage
 
 		try 
 		{
-                    image = ImageIO.read(new File("src_img/img.png"));
+                    image = ImageIO.read(getClass().getClassLoader().getResource("img.png"));
 
 		} 
 		catch (Exception e) 
@@ -115,7 +124,11 @@ class mainpage
                             admin_login.init();                         
 			}
                         else if(source == aboutBtn){
-                            about.init();
+                            try {
+                                about.init();
+                            } catch (IOException ex) {
+                                Logger.getLogger(mainpage.class.getName()).log(Level.SEVERE, null, ex);
+                            }
                         }
                         else if(source == police_loginBtn){
                             police_login.init();
@@ -125,7 +138,10 @@ class mainpage
                         }
                         else if(source == register_applicationBtn){
                             register_application.init();
-                        }                
+                        }
+                        else if(source == track_complaintBtn){
+                            track_complaint_verification.init();
+                        }
 			else if(source == quitBtn){
                             System.exit(0);
 			}
@@ -133,39 +149,146 @@ class mainpage
 	}
 	
         public static void set_up_database() throws ClassNotFoundException, Exception {
-            String createTables = readToString("sql/tables.sql");
-            
-            String addValues = readToString("sql/insertVal.sql");
             
             System.out.println("Attempting to contact DB ... ");
 
             try {
-              Class.forName("org.hsqldb.jdbc.JDBCDriver");
+//              Class.forName("org.hsqldb.jdbc.JDBCDriver");
+                Class.forName("com.mysql.jdbc.Driver");
             } catch (ClassNotFoundException e) {
               throw e;
             }
             
             try {
-                // will create DB if does not exist
-                // "SA" is default user with hypersql
-                con = DriverManager.getConnection(connectionString, "SA", "");
+                con = DriverManager.getConnection("jdbc:mysql://65.1.1.8:3306/test","police","Policemgmt@7police");
 
-//                 create tables
-                con.createStatement().executeUpdate(createTables);
+                // set up tables
                 
-                String sql="SELECT TOP 1 1 FROM police_dept";
+                String sql_police_dept = "CREATE TABLE if not exists\n" +
+"police_dept (\n" +
+"		  PD_ID int(11) AUTO_INCREMENT,\n" +
+"			PD_NAME varchar(45) DEFAULT NULL,\n" +
+"			PD_LOC varchar(45) DEFAULT NULL,\n" +
+"		  PD_CITY varchar(45) DEFAULT NULL,\n" +
+"		  PD_AREA varchar(45) DEFAULT NULL,\n" +
+"		  PD_PHONE varchar(45) DEFAULT NULL,\n" +
+"		  PD_REMARKS varchar(45) DEFAULT NULL,\n" +
+"			PRIMARY KEY (PD_ID)\n" +
+");";
+                PreparedStatement pst_police_dept = con.prepareStatement(sql_police_dept);
+                
+                String sql_fir = "CREATE TABLE if not exists\n" +
+"fir (\n" +
+"		 FIR_ID int(11) AUTO_INCREMENT,\n" +
+"	   FIR_NAME varchar(45) DEFAULT NULL,\n" +
+"	   FIR_FNAME varchar(45) DEFAULT NULL,\n" +
+"	   FIR_EMAIL varchar(45) DEFAULT NULL,\n" +
+"	   FIR_CONTACT varchar(45) DEFAULT NULL,\n" +
+"	   FIR_DOI varchar(45) DEFAULT NULL,\n" +
+"		 FIR_PS varchar(45) DEFAULT NULL,\n" +
+"		 FIR_DEP varchar(45) DEFAULT NULL,\n" +
+"		 FIR_DESC varchar(200) DEFAULT NULL,\n" +
+"		 FIR_STAT INT DEFAULT NULL,\n" +
+"		 FIR_EMAIL_SENT INT DEFAULT NULL,\n" +
+"		 FIR_PO_ID varchar(45) DEFAULT NULL,\n" +
+"		 FIR_TRACK varchar(45) DEFAULT NULL,\n" +                        
+"		 PRIMARY KEY (FIR_ID)\n" +
+"	);";
+                PreparedStatement pst_fir = con.prepareStatement(sql_fir);
+                
+                String sql_police_officer = "CREATE TABLE if not exists police_officer (\n" +
+"		 PO_ID int(11) AUTO_INCREMENT,\n" +
+"	   PO_NAME varchar(45) DEFAULT NULL,\n" +
+"	   PO_DOB varchar(45) DEFAULT NULL,\n" +
+"	   PO_GENDER varchar(45) DEFAULT NULL,\n" +
+"	   PO_DEP varchar(45) DEFAULT NULL,\n" +
+"	   PO_EMAIL varchar(45) DEFAULT NULL,\n" +
+"		 PO_CONTACT varchar(45) DEFAULT NULL,\n" +
+"		 PO_PS varchar(45) DEFAULT NULL,\n" +
+"		 PO_DRUGS varchar(45) DEFAULT NULL,\n" +
+"		 PO_AID varchar(45) DEFAULT NULL,\n" +
+"		 PO_LENSES varchar(45) DEFAULT NULL,\n" +
+"		 PRIMARY KEY (PO_ID)\n" +
+"	);";
+                PreparedStatement pst_police_officer = con.prepareStatement(sql_police_officer);
+                
+                String sql_police_officer_list = "CREATE TABLE if not exists police_officer_list (\n" +
+"		   USERNAME varchar(45) DEFAULT NULL,\n" +
+"		   PASSWORD varchar(45) DEFAULT NULL\n" +
+"	);";
+                PreparedStatement pst_police_officer_list = con.prepareStatement(sql_police_officer_list);
+                
+                String sql_criminal = "CREATE TABLE if not exists criminal (\n" +
+"	CRIM_ID int(11) AUTO_INCREMENT,\n" +
+"	CRIM_NAME varchar(45) DEFAULT NULL,\n" +
+"	CRIM_AGE int(11) DEFAULT NULL,\n" +
+"	CRIM_GENDER varchar(45) DEFAULT NULL,\n" +
+"	CRIM_ADDRESS varchar(45) DEFAULT NULL,\n" +
+"	CRIM_CONTACT varchar(45) DEFAULT NULL,\n" +
+"	CRIM_DOC varchar(45) DEFAULT NULL,\n" +
+"	CRIM_DESC varchar(200) DEFAULT NULL,\n" +
+"	CRIM_CRIME_DEP varchar(45) DEFAULT NULL,\n" +
+"	CRIM_PS varchar(45) DEFAULT NULL,\n" +
+"	PRIMARY KEY (CRIM_ID)\n" +
+");";
+                PreparedStatement pst_criminal = con.prepareStatement(sql_criminal);
+                
+                String sql_applic = "CREATE TABLE if not exists applic (\n" +
+"	APP_ID int(11) AUTO_INCREMENT,\n" +
+"	APP_TYPE varchar(45) DEFAULT NULL,\n" +
+"	APP_NAM varchar(45) DEFAULT NULL,\n" +
+"	APP_FNAME varchar(45) DEFAULT NULL,\n" +
+"	APP_EMAIL varchar(45) DEFAULT NULL,\n" +
+"	APP_CONTACT varchar(45) DEFAULT NULL,\n" +
+"	APP_DOI varchar(45) DEFAULT NULL,\n" +
+"	APP_PAN varchar(45) DEFAULT NULL,\n" +
+"	APP_AADHAR varchar(45) DEFAULT NULL,\n" +
+"	APP_PO_NAME varchar(45) DEFAULT NULL,\n" +
+"	APP_PO_ID varchar(45) DEFAULT NULL,\n" +
+"	APP_PS varchar(45) DEFAULT NULL,\n" +
+"	PRIMARY KEY (APP_ID)\n" +
+");";
+                PreparedStatement pst_applic = con.prepareStatement(sql_applic);
+                
+                pst_police_dept.executeUpdate();
+                pst_fir.executeUpdate();
+                pst_police_officer.executeUpdate();
+                pst_police_officer_list.executeUpdate();
+                pst_criminal.executeUpdate();
+                pst_applic.executeUpdate();
+                
+                String sql="SELECT * FROM police_dept;";
                 PreparedStatement pst = con.prepareStatement(sql);
                 ResultSet rs=pst.executeQuery();
                 if(rs.next()){
                     System.out.println("Table is not empty and database is set already");
                 }
                 else{
-                    System.out.println("Table is not set, call insertTables.sql");
-                    con.createStatement().executeUpdate(addValues);
+                    System.out.println("Table is not set, set starting values");
+                    // fill in details if the tables are empty
+                    UUID idOne = UUID.randomUUID();
+                    String uid = idOne.toString();
+                    String sql_pd = "INSERT INTO police_dept (PD_NAME, PD_LOC, PD_CITY, PD_AREA, PD_PHONE, PD_REMARKS) VALUES ('Sirsa Mandi Police Station', 'Grid-4', 'Sirsa','Anaj Mandi Sirsa','9899898765','Some text about the police station');";
+                    String sql_f = "INSERT INTO fir (FIR_NAME, FIR_FNAME, FIR_EMAIL, FIR_CONTACT, FIR_DOI, FIR_PS, FIR_DEP, FIR_DESC, FIR_STAT, FIR_EMAIL_SENT, FIR_PO_ID, FIR_TRACK) VALUES ('Test', 'FatherTest', 'visitormanagementsystem23@gmail.com', '9999999999', '12022001', 'Sirsa Mandi Police Station', 'Cybercrime', 'This complaint is registered with regard to a fraud bank payment on my SBI debit card.', 0, 0, '1',?);";
+                    String sql_po = "INSERT INTO police_officer (PO_NAME, PO_DOB, PO_GENDER, PO_DEP, PO_EMAIL, PO_CONTACT, PO_PS, PO_DRUGS, PO_AID, PO_LENSES) VALUES ('TestPoliceOfficer', '12031987', 'Male', 'Cybercrime', 'test@gmail.com', '9898989897', 'Sirsa Mandi Police Station', 'No', 'No', 'Yes');";
+                    String sql_pol = "INSERT INTO police_officer_list values ('TestPoliceOfficer', 'test');";
+                    String sql_cri = "INSERT INTO criminal (CRIM_NAME, CRIM_AGE, CRIM_GENDER, CRIM_ADDRESS, CRIM_CONTACT, CRIM_DOC, CRIM_DESC, CRIM_CRIME_DEP, CRIM_PS) values ('rcb', '41', 'Male', 'bangalore','9087564321','211021','bank loot','Law and Order', 'Sirsa Mandi Police Station');";
+                    String sql_app = "INSERT INTO applic (APP_TYPE, APP_NAM, APP_FNAME, APP_EMAIL, APP_CONTACT, APP_DOI, APP_PAN, APP_AADHAR, APP_PO_NAME,  APP_PO_ID, APP_PS) values ('Law and order', 'random', 'kapoor','rk@gmail.com','9234516789','211090','AABEE1234F','123456780987','bond',2,'Sirsa Mandi Police Station');";
+                    PreparedStatement pst_pd = con.prepareStatement(sql_pd);
+                    PreparedStatement pst_f = con.prepareStatement(sql_f);
+                    pst_f.setString(1, uid);
+                    PreparedStatement pst_po = con.prepareStatement(sql_po);
+                    PreparedStatement pst_pol = con.prepareStatement(sql_pol);
+                    PreparedStatement pst_cri = con.prepareStatement(sql_cri);
+                    PreparedStatement pst_app = con.prepareStatement(sql_app);
+                    
+                    pst_pd.executeUpdate();
+                    pst_f.executeUpdate();
+                    pst_po.executeUpdate();
+                    pst_pol.executeUpdate();
+                    pst_cri.executeUpdate();
+                    pst_app.executeUpdate();
                 }
-                // add random values in tables
-//                con.createStatement().executeUpdate(addValues);
-
 
               } catch (SQLException e) {
                 throw e;
@@ -173,6 +296,8 @@ class mainpage
                 con.close();
               }
         }
+        
+        
 	static void init() throws IOException, Exception
 	{
 		try
@@ -193,9 +318,13 @@ class mainpage
 		init();
 	}
         
-        public static String readToString(String fname) throws Exception {
+        public static ArrayList<String> readToString(String fname) throws Exception {
             File file = new File(fname);
             String string = FileUtils.readFileToString(file, "utf-8");
-            return string;
+            String[] strSplit = string.split(";");
+            
+            ArrayList<String> sqlList = new ArrayList<String>(Arrays.asList(strSplit));
+            
+            return sqlList;
       }
 }
