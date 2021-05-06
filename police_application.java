f9aefe0a232b769ce16d8a2dbdee29abeceee304
noob-aViral <guptaavi352@gmail.com>
2021-05-06 13:21:31 +0530

    Update for new theme

--- police_application.java
+++ police_application.java
@@ -10,37 +10,44 @@ import java.lang.reflect.InvocationTargetException;
 public class police_application{
 	static JFrame frame;
 	static JButton btnSubmit, btnCancel;
-	static JPanel panel, panel1, panel2;
-	static JLabel lblDrugs, lblAids, lblName, lblLenses, lblDOB, lblEmail, lblContact, lblAddress, lblGender, lblDepartment;
+	static JPanel panel, panel1, panel2, mainpanel;
+	static JLabel lblspace, lblDrugs, lblAids, lblName, lblLenses, lblDOB, lblEmail, lblContact, lblAddress, lblGender, lblDepartment, lblheading;
 	static JTextField txtName, txtDOB, txtEmail, txtAddress, txtContact;
 	static JComboBox txtGender, txtDrugs, txtAids, txtLenses, txtDepartment;
 	static Border redline = BorderFactory.createLineBorder(Color.RED);
-	
+	static Border blackline = BorderFactory.createLineBorder(new Color(235,235,235));
+	static Font font1 = new Font("Calibri", Font.BOLD, 24);
+
 	public static void user_details()
 	{
 		frame = new JFrame("POLICE APPLICATION");
-		panel = new JPanel(new GridLayout(11,1,10,10));
-		panel1 = new JPanel(new GridLayout(11,1,10,10));
+		mainpanel = new JPanel(new BorderLayout(10,10));
+		panel = new JPanel(new GridLayout(11,1,0,0));
+		panel1 = new JPanel(new GridLayout(11,1,0,0));
 		panel2 = new JPanel(new GridLayout(1,3,0,0));
 
 		//**************************************************************************************************************
 		// USER DETAILS
-		lblName = new JLabel("<HTML><h3>Name: </h3></HTML>", JLabel.CENTER);
-		lblDOB = new JLabel("<HTML><h3>DOB: </h3></HTML>", JLabel.CENTER);
-		lblEmail = new JLabel("<HTML><h3>Email: </h3></HTML>", JLabel.CENTER);
-		lblContact = new JLabel("<HTML><h3>Phone: </h3></HTML>", JLabel.CENTER);
-		lblAddress = new JLabel("<HTML><h3>Address: </h3></HTML>", JLabel.CENTER);
-		lblGender = new JLabel("<HTML><h3>Gender: </h3></HTML>", JLabel.CENTER);
-		lblDepartment = new JLabel("<HTML><h3>Department: </h3></HTML>", JLabel.CENTER);
-		lblDrugs = new JLabel("<HTML><h3>Have you ever been dependant on drugs or alcohol?</h3></HTML>", JLabel.CENTER);
-		lblAids = new JLabel("<HTML><h3>Do you use hearing aids?</h3></HTML>", JLabel.CENTER);
-		lblLenses = new JLabel("<HTML><h3>Do you use glasses for your eyesight?</h3></HTML>", JLabel.CENTER);
+		lblheading = new JLabel("<HTML><h1>POLICE APPLICATION</h1></HTML>", JLabel.CENTER);
+		lblheading.setForeground(new Color(255,189,68));
 
-		btnCancel = new JButton("<HTML><h3>Cancel</h3></HTML>");
-		btnCancel.setBackground(new Color(255,92,96));
+		lblName = new JLabel("<HTML><h2>Name: </h2></HTML>", JLabel.CENTER);
+		lblDOB = new JLabel("<HTML><h2>DOB: </h2></HTML>", JLabel.CENTER);
+		lblEmail = new JLabel("<HTML><h2>Email: </h2></HTML>", JLabel.CENTER);
+		lblContact = new JLabel("<HTML><h2>Phone: </h2></HTML>", JLabel.CENTER);
+		lblAddress = new JLabel("<HTML><h2>Address: </h2></HTML>", JLabel.CENTER);
+		lblGender = new JLabel("<HTML><h2>Gender: </h2></HTML>", JLabel.CENTER);
+		lblDepartment = new JLabel("<HTML><h2>Department: </h2></HTML>", JLabel.CENTER);
+		lblDrugs = new JLabel("<HTML><h2>Dependant on drugs or alcohol?</h2></HTML>", JLabel.CENTER);
+		lblAids = new JLabel("<HTML><h2>Do you use hearing aids?</h2></HTML>", JLabel.CENTER);
+		lblLenses = new JLabel("<HTML><h2>Do you use glasses for your eyesight?</h2></HTML>", JLabel.CENTER);
+		lblspace = new JLabel("				    ", JLabel.CENTER);
 
-		btnSubmit = new JButton("<HTML><h3>Submit</h3></HTML>");
-		btnSubmit.setBackground(new Color(52,119,235));
+		btnCancel = new JButton("<HTML><h2>Back</h2></HTML>");
+		btnCancel.setBackground(new Color(1, 145, 135));
+
+		btnSubmit = new JButton("<HTML><h2>Submit</h2></HTML>");
+		btnSubmit.setBackground(new Color(1, 145, 135));
 		
 		// btnCancel.addActionListener(new CustomActionListener());
 		// btnSubmit.addActionListener(new CustomActionListener());
@@ -73,6 +80,24 @@ public class police_application{
 		txtEmail.setInputVerifier(new PassVerifier());
 		txtContact.setInputVerifier(new PassVerifier());
 
+
+		lblName.setBorder(blackline);
+		lblDOB.setBorder(blackline);
+		lblEmail.setBorder(blackline);
+		lblContact.setBorder(blackline);
+		lblAddress.setBorder(blackline);
+		lblGender.setBorder(blackline);
+		lblDepartment.setBorder(blackline);
+		lblDrugs.setBorder(blackline);
+		lblAids.setBorder(blackline);
+		lblLenses.setBorder(blackline);
+
+		txtName.setBorder(blackline);
+		txtDOB.setBorder(blackline);
+		txtEmail.setBorder(blackline);
+		txtContact.setBorder(blackline);
+		txtAddress.setBorder(blackline);
+
 		panel2.add(new JLabel("<HTML><h1>POLICE APPLICATION</h1></HTML>", JLabel.CENTER));
 		panel.add(lblName);			panel1.add(txtName);
 		panel.add(lblDOB);			panel1.add(txtDOB);
@@ -84,13 +109,19 @@ public class police_application{
 		panel.add(lblDrugs);		panel1.add(txtDrugs);
 		panel.add(lblAids);			panel1.add(txtAids);
 		panel.add(lblLenses);		panel1.add(txtLenses);
-		panel.add(btnCancel);		panel1.add(btnSubmit);
 		panel2.add(panel);
 		panel2.add(panel1);
 		// panel.setBackground(new Color(45, 45, 45));
 		panel2.setBackground(new Color(255,189,68));
 
-		frame.setContentPane(panel2);
+		mainpanel.add(lblheading, BorderLayout.NORTH);
+		mainpanel.add(panel2, BorderLayout.CENTER);
+		mainpanel.add(btnCancel, BorderLayout.LINE_START);
+		mainpanel.add(lblspace, BorderLayout.SOUTH);
+		mainpanel.add(btnSubmit, BorderLayout.LINE_END);
+		mainpanel.setBackground(new Color(45,45,45));
+
+		frame.setContentPane(mainpanel);
 		frame.setSize(1920,1080);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
