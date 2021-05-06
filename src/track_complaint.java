f868bf13f95f5a983b27310966377be8991b3228
Bhupesh <66639467+BSardana01@users.noreply.github.com>
2021-05-06 17:28:45 +0530

    Update track_complaint.java

--- src/track_complaint.java
+++ src/track_complaint.java
@@ -22,14 +22,15 @@ public class track_complaint{
 	static JFrame frame;
 	static JButton btnSubmit, btnCancel;
 
-	static JPanel panel, panel1, panel2;
+	static JPanel panel, panel1, panel2, mainpanel;
 
 //	static JTextArea txtArea;
 	
-	static JLabel lblFatherName, lblName, lblEmail, lblContact, lblDOI, lblPS, lblDepartment, lblDesc, lblStatus;
+	static JLabel lblspace, lblheading, lblFatherName, lblName, lblEmail, lblContact, lblDOI, lblPS, lblDepartment, lblDesc, lblStatus;
 	static JLabel txtName, txtFatherName, txtEmail, txtDOI, txtPS, txtContact, txtDepartment, txtArea, txtStatus;
 //	static JComboBox txtDepartment;
 	static Border redline = BorderFactory.createLineBorder(Color.RED);
+        static Border blackline = BorderFactory.createLineBorder(new Color(235,235,235));
         
         static String name, fname, contact, email, doi, ps, dep, desc, status;
         static int stat;
@@ -38,39 +39,33 @@ public class track_complaint{
 	public static void user_details(String uid) throws ClassNotFoundException, SQLException
 	{   
                 System.out.println("uid in user_details: " + uid);
-		frame = new JFrame("FILL DETAILS");
-		panel = new JPanel(new GridLayout(12,1,10,10));
-		panel1 = new JPanel(new GridLayout(10,1,10,10));
+		frame = new JFrame("TRACK COMPLAINT");
+		mainpanel = new JPanel(new BorderLayout(10,10));
+		panel = new JPanel(new GridLayout(9,1,0,0));
+		panel1 = new JPanel(new GridLayout(9,1,0,0));
 		panel2 = new JPanel(new GridLayout(1,3,0,0));
 
 		//**************************************************************************************************************
 		// USER DETAILS
-			lblName = new JLabel("<HTML><h3>Name: </h3></HTML>", JLabel.CENTER);
-			lblFatherName = new JLabel("<HTML><h3>Father's / Husband's Name: </h3></HTML>", JLabel.CENTER);
-			lblEmail = new JLabel("<HTML><h3>Email: </h3></HTML>", JLabel.CENTER);
-			lblContact = new JLabel("<HTML><h3>Phone: </h3></HTML>", JLabel.CENTER);
-			lblDOI = new JLabel("<HTML><h3>Date of Issue: </h3></HTML>", JLabel.CENTER);
-			lblPS = new JLabel("<HTML><h3>Name of police station </h3></HTML>", JLabel.CENTER);
-			lblDepartment = new JLabel("<HTML><h3>Department: </h3></HTML>", JLabel.CENTER);
-			lblDesc  = new JLabel("<HTML><h3>Description: </h3></HTML>", JLabel.CENTER);
-                        lblStatus  = new JLabel("<HTML><h3>Status of complaint: </h3></HTML>", JLabel.CENTER);
+                        lblheading = new JLabel("<HTML><h1>TRACK COMPLAINT </h1></HTML>", JLabel.CENTER);
+                        lblheading.setForeground(new Color(255,189,68));
+			lblName = new JLabel("<HTML><h2>Name: </h2></HTML>", JLabel.CENTER);
+			lblFatherName = new JLabel("<HTML><h2>Father's / Husband's Name: </h2></HTML>", JLabel.CENTER);
+			lblEmail = new JLabel("<HTML><h2>Email: </h2></HTML>", JLabel.CENTER);
+			lblContact = new JLabel("<HTML><h2>Phone: </h2></HTML>", JLabel.CENTER);
+			lblDOI = new JLabel("<HTML><h2>Date of Issue: </h2></HTML>", JLabel.CENTER);
+			lblPS = new JLabel("<HTML><h2>Name of police station </h2></HTML>", JLabel.CENTER);
+			lblDepartment = new JLabel("<HTML><h2>Department: </h2></HTML>", JLabel.CENTER);
+			lblDesc  = new JLabel("<HTML><h2>Description: </h2></HTML>", JLabel.CENTER);
+                        lblStatus  = new JLabel("<HTML><h2>Status of complaint: </h2></HTML>", JLabel.CENTER);
+                        lblspace = new JLabel("				    ", JLabel.CENTER);
                         
 			btnCancel = new JButton("<HTML><h3>Cancel</h3></HTML>");
-			btnCancel.setBackground(new Color(255,92,96));
+			btnCancel.setBackground(new Color(1, 145, 135));
 
-//			btnSubmit = new JButton("<HTML><h3>Submit</h3></HTML>");
-//			btnSubmit.setBackground(new Color(52,119,235));
 			
 			btnCancel.addActionListener(new CustomActionListener());
-//			btnSubmit.addActionListener(new CustomActionListener());
 
-//			txtArea = new JTextArea(5, 20);
-//			JScrollPane scrollPane = new JScrollPane(txtArea); 
-//			txtArea.setEditable(true);
-
-//			String txtDepartmentOptions[] = {"Women Protection", "Cybercrime", "Traffic & Control", "Law and Order"};
-//			txtDepartment = new JComboBox(txtDepartmentOptions);
-//			txtDepartment.setSelectedIndex(0);
                         txtName = new JLabel("test");
 			txtFatherName = new JLabel("test");
 			txtEmail = new JLabel("test");
@@ -80,19 +75,11 @@ public class track_complaint{
                         txtArea = new JLabel("test");
                         txtDepartment = new JLabel("test");
                         txtStatus = new JLabel("test");
-
-			panel2.add(new JLabel("<HTML><h1>What is a police complaint?</h1><h3>A Police Complaint is initial reporting of any crime or offence. "
-                                + "It is a narration of facts about the incident in layman’s words.</h3><h1>When can a police complaint be filed?</h1><h3>One should "
-                                + "ideally file a police complaint immediately after the incident/crime have taken place. In certain cases of sexual harassment, "
-                                + "domestic violence, rape, etc. women usually take time to come out and report the crime/incident due to mental trauma. "
-                                + "In such cases, even if there is a delay one can file a police complaint.</h3><h1>Who can file a Police Complaint?</h1><h3>Anyone "
-                                + "can file a police complaint. The victim of the crime, victim’s family members, friends or any witness to the crime can file a police "
-                                + "complaint.</h3></HTML>", JLabel.CENTER));
-//                        panel2.add(stat);
 //                        panel2.add("jProgressBar", jProgressBar);
                         
                         ArrayList<String> values = get_values(uid);
                         
+                        panel2.add(new JLabel("<HTML><h1>TRACK COMPLAINT</h1></HTML>", JLabel.CENTER));
 			panel.add(lblName);			panel1.add(txtName);
 			panel.add(lblFatherName);	panel1.add(txtFatherName);
 			panel.add(lblEmail);		panel1.add(txtEmail);
@@ -102,31 +89,46 @@ public class track_complaint{
 			panel.add(lblDepartment);	panel1.add(txtDepartment);
 			panel.add(lblDesc);             panel1.add(txtArea);
                         panel.add(lblStatus);           panel1.add(txtStatus);                  
-			panel.add(btnCancel);		
-			panel2.add(panel);
+			
+                        lblName.setBorder(blackline);			txtName.setBorder(blackline);
+                        lblFatherName.setBorder(blackline);	txtFatherName.setBorder(blackline);
+                        lblEmail.setBorder(blackline);		txtEmail.setBorder(blackline);
+                        lblContact.setBorder(blackline);		txtContact.setBorder(blackline);
+                        lblDOI.setBorder(blackline);			txtDOI.setBorder(blackline);
+                        lblPS.setBorder(blackline);			txtPS.setBorder(blackline);
+                        lblDepartment.setBorder(blackline);	txtDepartment.setBorder(blackline);
+                        lblDesc.setBorder(blackline);			txtArea.setBorder(blackline);
+                        lblStatus.setBorder(blackline);		txtStatus.setBorder(blackline);
+			
                         
-			panel2.add(panel1);
-			// panel.setBackground(new Color(45, 45, 45));
-			panel2.setBackground(new Color(255,189,68));
+			panel2.add(panel);
+                        panel2.add(panel1);
+                        panel2.setBackground(new Color(255,189,68));
 
-			frame.setContentPane(panel2);
+
+                        mainpanel.add(lblheading, BorderLayout.NORTH);
+                        mainpanel.add(panel2, BorderLayout.CENTER);
+                        mainpanel.add(btnCancel, BorderLayout.LINE_START);
+                        mainpanel.add(lblspace, BorderLayout.SOUTH);
+                        mainpanel.add(new JLabel("		 ", JLabel.CENTER), BorderLayout.LINE_END);
+
+
+                        mainpanel.setBackground(new Color(45,45,45));
+                        frame.setContentPane(mainpanel);
                         
 			frame.setSize(1920,1080);
 			frame.setVisible(true);
 			frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
                         
-            for(String x : values){
-                System.out.println(x);
-            }
-                        txtName.setText(values.get(0));
-			txtFatherName.setText(values.get(1));
-			txtEmail.setText(values.get(2));
-			txtContact.setText(values.get(3));
-			txtDOI.setText(values.get(4));
-			txtPS.setText(values.get(5));
-                        txtArea.setText(values.get(6));
-                        txtDepartment.setText(values.get(7));
-                        txtStatus.setText(values.get(8));
+                        txtName.setText("<HTML><h3>" + values.get(0)  + "</h3></HTML>");
+			txtFatherName.setText("<HTML><h3>" + values.get(1) + "</h3></HTML>");
+			txtEmail.setText("<HTML><h3>" + values.get(2)  + "</h3></HTML>");
+			txtContact.setText("<HTML><h3>" + values.get(3)  + "</h3></HTML>");
+			txtDOI.setText("<HTML><h3>" + values.get(4)  + "</h3></HTML>");
+			txtPS.setText("<HTML><h3>" + values.get(5)  + "</h3></HTML>");
+                        txtArea.setText("<HTML><h3>" + values.get(6)  + "</h3></HTML>");
+                        txtDepartment.setText("<HTML><h3>" + values.get(7)  + "</h3></HTML>");
+                        txtStatus.setText("<HTML><h3>" + values.get(8)  + "</h3></HTML>");
 		//**************************************************************************************************************
 	}
         
@@ -150,7 +152,7 @@ public class track_complaint{
             try {
                 con = DriverManager.getConnection("jdbc:mysql://65.1.1.8:3306/test","police","Policemgmt@7police");
                 System.out.println("Got uid: " + uid);
-                String sql_fir="select FIR_NAME, FIR_FNAME, FIR_EMAIL, FIR_CONTACT, FIR_DOI, FIR_PS, FIR_DEP, FIR_DESC, FIR_STAT from fir where FIR_TRACK=?";
+                String sql_fir="select FIR_NAME, FIR_FNAME, FIR_EMAIL, FIR_CONTACT, FIR_DOI, FIR_PS, FIR_DESC, FIR_DEP, FIR_STAT from fir where FIR_TRACK=?";
                 PreparedStatement pst_fir = con.prepareStatement(sql_fir);
                 pst_fir.setString(1, uid);
                 
@@ -162,8 +164,8 @@ public class track_complaint{
                     contact = rs_fir.getString(4);
                     doi = rs_fir.getString(5);
                     ps = rs_fir.getString(6);
-                    dep = rs_fir.getString(7);
-                    desc = rs_fir.getString(8);
+                    desc = rs_fir.getString(7);
+                    dep = rs_fir.getString(8);
                     stat = rs_fir.getInt(9);
                 }
                 System.out.println("Got name: " + name);
@@ -179,8 +181,8 @@ public class track_complaint{
                 values.add(contact);
                 values.add(doi);
                 values.add(ps);
-                values.add(dep);
                 values.add(desc);
+                values.add(dep);
                 if(stat == 1){
                     status = "Solved";
                 }
